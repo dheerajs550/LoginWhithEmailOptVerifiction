cea45e0c35ff437d5def2ac807d179e20ab297a4
Dheeraj sharma <dheerajsharma550@gmail.com>
2025-09-14 16:46:34 +0530

    chang in email .config code  by dheeraj for email otp login

--- middleware/Emailconfig.js
+++ middleware/Emailconfig.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer'
 // Create a test account or replace with real credentials.
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
    user: process.env.EMAIL_USER,  // Gmail address
