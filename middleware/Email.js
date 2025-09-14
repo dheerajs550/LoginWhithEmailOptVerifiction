95fc6db9959674e85cedb9f470a42703cdb4ef9f
Dheeraj sharma <dheerajsharma550@gmail.com>
2025-09-14 16:42:45 +0530

    chang in email .config code  by dheeraj for email otp login

--- middleware/Email.js
+++ middleware/Email.js
@@ -1,33 +1,32 @@
+import  { getEmailTemplate, getWelcomeTemplate } from "../libs/EmailTemplate.js";
+import { transporter } from "./Emailconfig.js";
 
-import { Resend } from "resend";
-import { getEmailTemplate, getWelcomeTemplate } from "../libs/EmailTemplate.js";
+export const SendVerfionCode=async(email, verficationCode,name)=>{
+    try {
+         const response = await transporter.sendMail({
+                       from: '"Maddison Foo Koch" <dheerajsharma550@gmail.com>',
+                       to: email,
+                       subject: "Verify your Email ✔",
+                       text: "jay shree ram.....", // plain‑text body
+                       html: getEmailTemplate(name,verficationCode), // HTML body
+                     });
+                     console.log("Email send successfully",response)
+    } catch (error) {
+        console.log("Email error")
+    }
+}
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export const SendVerfionCode = async (email, verficationCode, name) => {
-  try {
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",  // free domain from Resend
-      to: email,
-      subject: "Verify your Email ✔",
-      html: getEmailTemplate(name, verficationCode),
-    });
-    console.log("Verification email sent:", response);
-  } catch (error) {
-    console.error("Email error:", error);
-  }
-};
-
-export const welcomeEmail = async (email, name) => {
-  try {
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: email,
-      subject: "Welcome Email ✔",
-      html: getWelcomeTemplate(name),
-    });
-    console.log("Welcome email sent:", response);
-  } catch (error) {
-    console.error("Welcome email error:", error);
-  }
-};
+export const welcomeEmail=async(email,name)=>{
+    try {
+         const response = await transporter.sendMail({
+                       from: '"Maddison Foo Koch" <dheerajsharma550@gmail.com>',
+                       to: email,
+                       subject: "Welcome Email ✔",
+                       text: "WelCome Email", // plain‑text body
+                       html: getWelcomeTemplate(name), // HTML body
+                     });
+                     console.log("Email send successfully",response)
+    } catch (error) {
+        console.log("Email error")
+    }
+}
--- middleware/Emailconfig.js
+++ middleware/Emailconfig.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer'
 // Create a test account or replace with real credentials.
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false, // true for 465, false for other ports
   auth: {
    user: process.env.EMAIL_USER,  // Gmail address
